8db84b733dc56e382e48fcf98cd726238bb8cb54
qed11 <ericdai1130@gmail.com>
2022-04-01 14:53:03 -0400

    Update sandwich_fail.m

--- code/sandwich/sandwich_fail.m
+++ code/sandwich/sandwich_fail.m
@@ -15,12 +15,12 @@ tau_c = 792e3;
 s_c = 1.01e6;
 %all strength in Pa
 
-
-
 micro = 4 * b * d * t *s_f_comp/L
 core = 2*b*d*tau_c
 wrink = 2 * b * t * d/L * (E_f * E_c * G_c)^1/3
 indent_elas = b * t *(pi^2 * d * E_f * s_c^2/(3*L))^(1/3)
+%indent_plas not needed anymore because it is not applicable to our
+%material
 
 EI_eq = E_f*b*t*d^2/2 + E_f*b*t^3/6 + E_c*b*c^3/12;
 AG_eq = b*d^2*G_c/c;
@@ -31,4 +31,3 @@ fail = fail/1e3;
 P = min(fail)*1e3;
 
 delta = P*L^3/(48*EI_eq) + P*L/(4*AG_eq)
-
